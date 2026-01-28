c711c8a25228de8853da47709a7dc7565950ac32
Harshit Jain <harshitjain2804@gmail.com>
2026-01-28 18:25:54 +0530

    Updated Gold component

--- src/components/Gold.jsx
+++ src/components/Gold.jsx
@@ -1,7 +1,7 @@
 import { GOLD_IMG_URL } from "../utils/app.constants";
 const Gold = () => {
     return (
-    <div className="bg-black relative py-[15rem] mt-[20rem] text-white flex items-center justify-center flex-col">
+    <div className="bg-black relative py-60 mt-80 text-white flex items-center justify-center flex-col">
         <img src={GOLD_IMG_URL} alt="gold-img" />
     </div>
     );
