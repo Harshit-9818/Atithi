8570819474b9270d62f698efdeca010c4d5ef56f
Harshit Jain <harshitjain2804@gmail.com>
2026-01-28 18:22:55 +0530

    Udated Gold Component

--- src/components/Gold.jsx
+++ src/components/Gold.jsx
@@ -1,7 +1,8 @@
+import { GOLD_IMG_URL } from "../utils/app.constants";
 const Gold = () => {
     return (
-    <div>
-        Gold Component
+    <div className="bg-black relative py-[15rem] mt-[20rem] text-white flex items-center justify-center flex-col">
+        <img src={GOLD_IMG_URL} alt="gold-img" />
     </div>
     );
 };
