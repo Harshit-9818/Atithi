55a0fed647dbf74e43b6683b25af9fa11bbf8358
Harshit Jain <harshitjain2804@gmail.com>
2026-01-28 19:11:54 +0530

    Scrollbar hidden and Gold Component Created

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import Gold from "./components/Gold";
 import HeroSection from "./components/HeroSection";
 import OrderSection from "./components/OrderSection";
 import Snowfall from "react-snowfall";
+import './index.css'
 const App = () => {
   return (
-    <div className="relative w-screen h-full">
+    <div className="relative w-screen h-screen overflow-x-hidden no-scrollbar">
       <Snowfall color="#82C3D9"/>
       <HeroSection />
       <OrderSection />
--- src/components/Gold.jsx
+++ src/components/Gold.jsx
@@ -2,7 +2,7 @@ import { GOLD_IMG_URL } from "../utils/app.constants";
 const Gold = () => {
     return (
     <div className="bg-black relative py-60 mt-80 text-white flex items-center justify-center flex-col">
-        <img src={GOLD_IMG_URL} alt="gold-img" />
+        <img src={GOLD_IMG_URL} alt="gold-img" className="w-76.5 h-33.5"/>
     </div>
     );
 };
