dbdbf15188f02408880bea350677c36ff781ad42
Harshit Jain <harshitjain2804@gmail.com>
2026-01-28 19:22:39 +0530

    Updated Gold component to include image and styling

--- src/components/Gold.jsx
+++ src/components/Gold.jsx
@@ -3,6 +3,14 @@ const Gold = () => {
     return (
     <div className="bg-black relative py-60 mt-80 text-white flex items-center justify-center flex-col">
         <img src={GOLD_IMG_URL} alt="gold-img" className="w-76.5 h-33.5"/>
+        <div className="mt-6 text-xl flex flex-col font-normal bg-linear-to-r from-[#8C6115] via-[#E9c789] to-[#996F25] bg-clip-text text-center text-transparent">
+            <span>
+                India's Top Savings
+            </span>
+            <spn>
+                Program for Food Lovers
+            </spn>
+        </div>
     </div>
     );
 };
