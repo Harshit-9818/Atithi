d294ef91c900e98d612af9cd9f298217b8b0af67
Harshit Jain <harshitjain2804@gmail.com>
2026-01-29 18:47:01 +0530

    Gold.jsx completed

--- src/components/Gold.jsx
+++ src/components/Gold.jsx
@@ -1,5 +1,4 @@
-import { GOLD_IMG_URL } from "../utils/app.constants";
-import GoldStar from "./GoldStar";
+import { FREE_DELIVERY_IMG_URL, GOLD_IMG_URL, GOLD_STAR_IMG_URL, SCOTTER_IMG_URL } from "../utils/app.constants";
 const Gold = () => {
     return (
     <div className="bg-black relative py-60 mt-80 text-white flex items-center justify-center flex-col">
@@ -17,6 +16,25 @@ const Gold = () => {
             <span className="text-2xl font-medium tracking-[3px]">GOLD BENEFITS</span>
             <span className="text-lg bg-linear-to-r from-[#8C6115] via-[#E9c789] to-[#996F25] bg-clip-text text-center text-transparent">&#9733;</span>
         </div>
+        <div className="flex gap-10 mt-8">
+            <div className="flex items-center justify-center gap-5">
+                <img src={FREE_DELIVERY_IMG_URL} alt="Free-Delivery-Image" className="w-[4.6rem]"/>
+                <div className="flex flex-col">
+                    <span className="text-2xl">Free Delivery</span>
+                    <span className="text-xl mt-1 bg-linear-to-r from-[#8C6115] via-[#E9c789] to-[#996F25] bg-clip-text text-center text-transparent">At All Restaurants within 7 km</span>
+                </div>
+            </div>
+            <div className="flex items-center justify-center gap-5">
+                <img src={SCOTTER_IMG_URL} alt="Scotter-Image" className="w-[4.6rem]"/>
+                <div className="flex flex-col">
+                    <span className="text-2xl">Up to 30% extra off</span>
+                    <span className="text-xl mt-1 bg-linear-to-r from-[#8C6115] via-[#E9c789] to-[#996F25] bg-clip-text text-center text-transparent">At 20,000+ partner restaurants</span>
+                </div>
+            </div>
+
+        </div>
+        <img src={GOLD_STAR_IMG_URL} alt="gold-star-img" className="w-44 absolute top-0 left-0"/>
+        <img src={GOLD_STAR_IMG_URL} alt="gold-star-img" className="w-76 absolute top-0 right-0 rotate-1"/>
     </div>
     );
 };
